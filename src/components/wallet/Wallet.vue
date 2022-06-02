86ff820d79b6ee8da690c95d4c8cbccf27933ee7
sirgallo <nickgallo97@gmail.com>
2022-06-02 11:56:09 -0400

    scroll and wallet

--- src/components/wallet/Wallet.vue
+++ src/components/wallet/Wallet.vue
@@ -3,5 +3,9 @@
 </script>
 
 <template>
-  <wallet-multi-button dark></wallet-multi-button>
+  <wallet-multi-button></wallet-multi-button>
 </template>
+
+<style lang="scss">
+  @import '@components/wallet/Wallet.scss';
+</style>
