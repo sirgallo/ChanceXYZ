6993183e1e13245e03683ae1b167edd1455634cb
sirgallo <nickgallo97@gmail.com>
2022-06-03 08:58:51 -0400

    good for now

--- src/components/network/NetworkStats.vue
+++ src/components/network/NetworkStats.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import { onMounted } from 'vue';
+  import { ref, onMounted } from 'vue';
   import { storeToRefs } from 'pinia';
   import { useNetworkStore } from '@stores/network';
 
@@ -7,8 +7,22 @@
 
   const { cluster, tps } = storeToRefs(networkStore);
 
+  const stable = ref(true);
+
+  function isNetworkStable() {
+    if (tps.value < 1300) {
+      console.log('here', tps);
+      stable.value = false;
+    } else {
+      stable.value = true;
+    }
+  }
+
   onMounted(() => {
-    setTimeout(async () => await networkStore.setPerformanceMetrics(), 2000);
+    setTimeout(async () => { 
+      await networkStore.setPerformanceMetrics();
+      isNetworkStable();
+    }, 2000);
   });
 </script>
 
@@ -16,7 +30,8 @@
   <div class="network-stats-container">
     <div class="network-stats-layer-base"></div>
     <div class="network-stats-layer-top">
-      <h4><b>{{ cluster.toUpperCase() }}</b> | Current TPS: {{ tps }} <i class="fa-solid fa-circle blinker"></i></h4>
+      <h4 v-if="stable"><b>{{ cluster.toUpperCase() }}</b> | Current TPS: {{ tps }} <i class="fa-solid fa-circle blinker-okay"></i></h4>
+      <h4 v-else><b>{{ cluster.toUpperCase() }}</b> | Current TPS: {{ tps }} <i class="fa-solid fa-circle blinker-warn"></i></h4>
     </div>
   </div>
 </template>
